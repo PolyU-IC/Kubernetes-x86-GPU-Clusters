c01acee6e469923c1b53a041ed791f2fe99e60ee
vincent51689453 <vincent51689453@gmail.com>
2020-05-21 15:46:23 +0800

    master setup

---
+++ tools/install_gtx1650_430.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install ubuntu-drivers-common
+sudo apt install nvidia-430
---
+++ tools/join_cluster.sh
@@ -0,0 +1,2 @@
+sudo kubeadm join 158.132.163.61:6443 --token er66bo.yi1wp01930zapyao \
+    --discovery-token-ca-cert-hash sha256:aef9d207e4dcc8a7179321e6531ddbf739d9ef9a94fc67969cccb3d97a05ca08 

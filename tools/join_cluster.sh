ed5fdc3c114788a41bd165bd26d4cfb466777cdf
vincent51689453 <vincent51689453@gmail.com>
2020-05-27 11:07:50 +0800

    metrices server testing

--- tools/join_cluster.sh
+++ tools/join_cluster.sh
@@ -1,2 +1,2 @@
-sudo kubeadm join 158.132.163.155:6443 --token f9dc58.tlphf21pad8iffvb \
-    --discovery-token-ca-cert-hash sha256:0672fab96d2dcea4043990993d9c6191ca7f66fd164783fd9d156e34faee019b
+sudo kubeadm join 158.132.163.155:6443 --token 27x14j.dz66ytqzlcxfucxy \
+    --discovery-token-ca-cert-hash sha256:3fe48ba67436155eea7bff3c6cce1d8325a0f4380804885d8739c542eb19e5e5

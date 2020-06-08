3b5a0cf94dc74c457ad7538af3c9b796228498b3
vincent51689453 <vincent51689453@gmail.com>
2020-06-08 15:25:18 +0800

    update cluster token

--- tools/join_cluster.sh
+++ tools/join_cluster.sh
@@ -1,2 +1,2 @@
-sudo kubeadm join 158.132.153.196:6443 --token bsqk1f.yjhu8rmg9qsrv39q \
-    --discovery-token-ca-cert-hash sha256:fa9e08332efc036d004d95166d9c0f377d94ce5339e13b0306ec4980e328d290
+sudo kubeadm join 158.132.153.196:6443 --token n5hb45.9covi9npp9dumeo7 \
+    --discovery-token-ca-cert-hash sha256:6e115dc3425fed45307020a28efabb9970534c184dd8ba5fbe7995749bc63f32
